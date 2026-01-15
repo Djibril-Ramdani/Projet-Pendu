562155dfe682349e835f8e6494d017b8d617a3cd
DjibSan <ramdjibril24@gmail.com>
2026-01-15 17:57:03 +0100

    On débute HelloMessage

--- protocol/HelloMessage.java
+++ protocol/HelloMessage.java
@@ -1,4 +1,34 @@
 package protocol;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class HelloMessage {
+    private String adresseIpGameMaster;
+    private int adressePort;
+
+    public HelloMessage(String adresseIpGameMaster, int adressePort) throws IOException {
+        this.adresseIpGameMaster = adresseIpGameMaster;
+        this.adressePort = adressePort;
+        if (adresseIpGameMaster == null)
+            System.out.println("Adresse IP Game Master is null");
+        else if (adressePort == 2025 ) {
+            System.out.println("Le port doit être différent de 2025");
+
+        }
+
+        Socket socket = new Socket("adresseIpGameMaster", adressePort);
+
+        OutputStream output = socket.getOutputStream();
+
+        BufferedWriter test = new BufferedWriter(new OutputStreamWriter(output));
+
+        test.write("HELLO");
+        test.newLine();
+        test.flush();
+
+    }
 }
